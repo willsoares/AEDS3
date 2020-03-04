afe580d473d3409f16b6d1f7fb28ad6e07dfebfa
willsoares <willsoares96@hotmail.com>
2020-03-03 23:13:14 -0300

    Translated a bubble sort algorithim in C to Java

---
+++ tp2/src/main/java/aeds3/App.java
@@ -0,0 +1,44 @@
+package aeds3;
+
+/**
+ * Willian de Souza Soares 2014.1.08.034 
+ * Intentionally made with static methods
+ */
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[] { 7, 6, 5, 4, 3, 2, 1 };
+        System.out.println("Un-ordered array: ");
+        printArray(array);
+
+        sortArray(array);
+        System.out.println("Ordered array: ");
+        printArray(array);
+    }
+
+    public static void sortArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int aux = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = aux;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1)
+                System.out.print(", ");
+            else
+                System.out.println("]");
+        }
+    }
+}
